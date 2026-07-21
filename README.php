335e153e150a19047bc7bafd1d4b00c225cce5f7
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-07-21 21:28:05 +0530

    Update README.php

--- README.php
+++ README.php
@@ -1,1 +1,18 @@
- 
+ FUNCTION isSubsequence(s, t)
+
+    i = 0
+    j = 0
+
+    WHILE i < length of s AND j < length of t
+
+        IF s[i] == t[j]
+            i = i + 1
+        END IF
+
+        j = j + 1
+
+    END WHILE
+
+    RETURN i == length of s
+
+END FUNCTION
